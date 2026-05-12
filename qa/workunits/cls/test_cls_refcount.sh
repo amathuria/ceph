cbf2f315b301d6b048f296173714c04d5e721eb8
Aishwarya Mathuria <amathuri@redhat.com>
2026-05-12 11:08:11 +0530

    Disable ec-data-pool in rgw crimson
    Signed-off-by: Aishwarya Mathuria <amathuri@redhat.com>

--- qa/workunits/cls/test_cls_refcount.sh
+++ qa/workunits/cls/test_cls_refcount.sh
@@ -1,5 +1,13 @@
 #!/bin/sh -e
 
-ceph_test_cls_refcount
+# Crimson OSD currently crashes when servicing some EC-pool CLS ops in
+# cls_refcount (see osd_flavour/crimson). Skip the *_ec subtests when the
+# caller (teuthology workunit env) tells us to. The full suite still runs
+# under classic OSD.
+if [ -n "${CRIMSON_SKIP_EC_TESTS:-}" ]; then
+    ceph_test_cls_refcount --gtest_filter='-*_ec'
+else
+    ceph_test_cls_refcount
+fi
 
 exit 0
